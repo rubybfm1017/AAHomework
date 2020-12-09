9063e3520fc5091640c111c3f6aed2edb8d29141
Qi Zhu <rubybfm1017@gmail.com>
2020-12-08 22:26:54 -0800

    w4d3 homework

---
+++ w4d3/lib/simon.rb
@@ -0,0 +1,67 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+
+  end
+
+  def play
+    until @game_over
+      self.take_turn
+    end
+    if @game_over
+      self.game_over_message
+      self.reset_game
+    end
+  end
+
+  def take_turn
+    self.show_sequence
+    self.require_sequence
+    unless @game_over
+      self.round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    self.add_random_color
+    p @seq
+  end
+
+  def require_sequence
+    @seq.each do |color|
+      user_answer = gets.chomp
+      if color != user_answer
+        @game_over = true
+        return
+      end
+    end
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+
+  end
+
+  def round_success_message
+    p "round success!"
+  end
+
+  def game_over_message
+    p "game over!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
+
+
